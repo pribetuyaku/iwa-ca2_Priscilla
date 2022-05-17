50bebcbc7cd5a2bf0ec74860c51a7694961fafa6
pribetuyaku <pcgb.com@gmail.com>
2022-05-17 09:57:03 +0000

    MongoDB connection fix connection

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
+const mongoose = require('mongoose');
 
-//const connectDB = require('./server/database/connection');
 
 const app = express();
 //separate the crenditials
@@ -14,20 +14,19 @@ const PORT = process.env.PORT || 8080
 // log requests
 app.use(morgan('tiny'));
 
-app.get('/',(req, res)=>{
-    res.render('index');
-})
-
-app.get('/add_user',(req, res)=>{
-    res.render('add_user');
-})
-
 // mongodb connection
-//connectDB();
+mongoose.connect(process.env.MONGO_URI);
+
+mongoose.connection.on('error', (err) => {
+    console.log('Mongodb error ', err, '.');
+    process.exit();
+});
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB successfully connected.');
+});
 
 // parse request to body-parser
 app.use(bodyparser.urlencoded({ extended : true}))
-
 
 var cons = require('consolidate');
 
@@ -41,7 +40,8 @@ app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
+
 // load routers
-//app.use('/', require('./server/routes/router'))
+app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
--- server/routes/router.js
+++ server/routes/router.js
@@ -2,32 +2,15 @@ const express = require('express');
 const route = express.Router()
 
 const services = require('../services/render');
-const controller = require('../controller/controller');
+//const controller = require('../controller/controller');
 
-/**
- *  @description Root Route
- *  @method GET /
- */
+//root route - method GET
 route.get('/', services.homeRoutes);
 
-/**
- *  @description add users
- *  @method GET /add-user
- */
-route.get('/add-user', services.add_user)
+//add users - method GET
+route.get('/add_user',services.add_user);
 
-/**
- *  @description for update user
- *  @method GET /update-user
- */
-route.get('/update-user', services.update_user)
-
-
-// API
-route.post('/api/users', controller.create);
-route.get('/api/users', controller.find);
-route.put('/api/users/:id', controller.update);
-route.delete('/api/users/:id', controller.delete);
-
+//update users - method GET
+route.get('/update-user', services.update_user);
 
 module.exports = route
